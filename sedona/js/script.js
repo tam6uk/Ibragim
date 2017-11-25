375b746d4d5e989a65ceacc7645c0738a8a1c7c5
ibragimtokmakov <tokmakovibragim@gmail.com>
2017-11-25 12:01:21 +0300

    Разметка сетки двух макетов
    Сделал разметку сетки двух личных проектов: sedona и gllacy.

--- sedona/js/script.js
+++ sedona/js/script.js
@@ -1,15 +1,17 @@
 var btn = document.querySelector('.main-form__btn'),
 		form = document.querySelector('.main-form-wrap');
 
-btn.addEventListener("click", function(e) {
-	var target = event.target;
-	if(target.classList.contains('btn-toggle')) {
-        form.classList.toggle('popup-hide');
-        form.classList.toggle('popup-show');
+if(btn) {
+      btn.addEventListener("click", function(e) {
+    	var target = event.target;
+    	if(target.classList.contains('btn-toggle')) {
+            form.classList.toggle('popup-hide');
+            form.classList.toggle('popup-show');
 
-        if(form.classList.contains('popup-show')) {
-          form.querySelector('input').focus();
-        }
-        return;
-   }
-});
+            if(form.classList.contains('popup-show')) {
+              form.querySelector('input').focus();
+            }
+            return;
+       }
+    });
+}
